116a5abfa8e46177c8e299805518e94ebf286f1d
Yusuke Ise <shosuke447@gmail.com>
2022-06-21 01:55:20 +0900

    create a Flowlist component.

--- pages/App.tsx
+++ pages/App.tsx
@@ -1,9 +1,11 @@
 import BOPMonth from './component/BOPMonth'
+import FlowList from './component/FlowList';
 
 export default function App(){
   return(
     <main style={{display: 'flex', justifyContent: 'center', backgroundColor: '#ffe1bd'}}>
       <BOPMonth />
+      <FlowList />
     </main>
   );
 }
---
+++ pages/component/FlowList.tsx
@@ -0,0 +1,7 @@
+export default function FlowList(){
+  return(
+    <div>
+      
+    </div>
+  );
+}
