fe1472519e771f4fc3292639d40fd1af3dbd22fa
Yusuke Ise <shosuke447@gmail.com>
2022-06-22 15:34:20 +0900

    install material-ui, add table to FlowList

--- pages/App.tsx
+++ pages/App.tsx
@@ -3,7 +3,7 @@ import FlowList from './component/FlowList';
 
 export default function App(){
   return(
-    <main style={{display: 'flex', justifyContent: 'center', backgroundColor: '#ffe1bd'}}>
+    <main style={{display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: '#ffe1bd'}}>
       <BOPMonth />
       <FlowList />
     </main>
--- pages/component/FlowList.tsx
+++ pages/component/FlowList.tsx
@@ -1,10 +1,60 @@
+import { useState } from "react";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { create } from "domain";
+
+function createData(
+  id: number,
+  date: number,
+  purchase: string,
+  category: string,
+  price: number
+){
+  return {id, date, purchase, category, price};
+}
+
 export default function FlowList(){
+  const columns = [
+    {title: '日付', field:'date'},
+    {title: '用途', field:'purchase'},
+    {title: 'カテゴリ', field:'category'},
+    {title: '金額', field:'price'}
+  ]
+  const [data, setdata] = useState([createData(1, 1, 'チョコレート', '食料品', 150)]);
   return(
-    <div>
+    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
       <h2>支出項目リスト</h2>
-      <div>
-        
-      </div>
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {columns.map((e) => (
+              <TableCell align="right">{e.title}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.date}
+              </TableCell>
+              <TableCell align="right">{row.purchase}</TableCell>
+              <TableCell align="right">{row.category}</TableCell>
+              <TableCell align="right">{row.price}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
     </div>
   );
 }
