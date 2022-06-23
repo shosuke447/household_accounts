5743488edf3a6b30cfba1b674eb428e2b576e654
Yusuke Ise <shosuke447@gmail.com>
2022-06-24 01:24:05 +0900

    change BOPMonth design

--- pages/component/BOPMonth.tsx
+++ pages/component/BOPMonth.tsx
@@ -15,18 +15,18 @@ const BOPMonth: FC<BOPProps> = ({income, expenditure}) => {
   }
 
   return(
-    <div style={{width: '80%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '1.8em', border: 'thick double maroon', backgroundColor: '#ffffff40'}}>
+    <div style={{width: '80%', display: 'flex', flexDirection: 'column', alignItems: 'center', margin: '1.8em', border: 'thick double maroon', backgroundColor: 'white' /*, backgroundColor: '#ffffff40'*/}}>
       <div>
         <h2>今月の収支</h2>
         <div style={{textAlign: 'center'}}><span style={{fontSize: '2em', color: bopColor}}>{bop}</span>円</div>
       </div>
       <div style={{width: '100%',display: 'flex', justifyContent: 'space-around'}}>
-        <div>
-          <h3>収入</h3>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+          <h3 style={{paddingRight: '1ex'}}>予算</h3>
           <div style={{textAlign: 'center', marginBottom: '1.8em'}}><span style={{fontSize: '2em'}}>{income}</span>円</div>
         </div>
-        <div>
-          <h3>支出</h3>
+        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+          <h3 style={{paddingRight: '1ex'}}>支出</h3>
           <div style={{textAlign: 'center', marginBottom: '1.8em'}}><span style={{fontSize: '2em'}}>{expenditure}</span>円</div>
         </div>
       </div>
--- pages/component/FlowList.tsx
+++ pages/component/FlowList.tsx
@@ -7,7 +7,6 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { Button } from "@mui/material";
-import { getFlowList } from "../../db/flowlist";
 
 type FlowListProps = {
   setExpenditure: Function
